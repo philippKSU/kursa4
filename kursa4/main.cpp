de8df6319448c16cc86d0bd9ae82e04109bb7ffc
philipp <philippapple@mail.ru>
2024-09-07 14:08:50 +0300

    Initial Commit

---
+++ kursa4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  kursa4
+//
+//  Created by филипп кудрявцев on 07.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
